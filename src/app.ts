d5c734f90527bc94d4446f595b0bec898683b767
Yulia Berezhnaya <p.kotta@yandex.ru>
2023-12-06 00:47:20 +0300

    debug

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,8 @@ import 'dotenv/config';
 import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
-// import { errors } from 'celebrate';
-// import cors from 'cors';
+import { errors } from 'celebrate';
+import cors from 'cors';
 import errorHandler from './middlewares/error-handler';
 import { DB_ADDRESS } from './config';
 import routes from './routes';
@@ -14,12 +14,12 @@ const app = express();
 mongoose.connect(DB_ADDRESS);
 
 // Только для локальных тестов. Не используйте это в продакшене
-// app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(routes);
-// app.use(errors());
+app.use(errors());
 app.use(errorHandler);
 
 // eslint-disable-next-line no-console
