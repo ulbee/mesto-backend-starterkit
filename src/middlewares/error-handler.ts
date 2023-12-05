6fa6d80dddf8d66615a81ab95d6c0ebc84535ad0
Yulia Berezhnaya <p.kotta@yandex.ru>
2023-12-06 00:17:35 +0300

    debug

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -2,9 +2,8 @@ import { ErrorRequestHandler } from 'express';
 
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   const { statusCode = 500 } = err;
-  console.log(`ERROR!!!!!!!!!!! ${err}`);
   const isErrorInternal = statusCode === 500;
-  const message = isErrorInternal ? 'На сервере произошла ошибка' : err.message;
+  const message = isErrorInternal ? `На сервере произошла ошибка ${JSON.stringify(err)}` : err.message;
   res.status(statusCode).send({ message });
   next();
 };
